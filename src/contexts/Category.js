1b8166f6f9a87024010a4d2068c30be1a2683cc9
himanshu khanal <himanshukhanal38320@gmail.com>
2023-04-11 19:53:59 +0530

    feat: ui changes for responsive

--- src/contexts/Category.js
+++ src/contexts/Category.js
@@ -6,7 +6,7 @@ const Category = ({ category }) => {
       <img
         src={category.image}
         alt={category.category}
-        className=" h-80 w-80 bg-center object-cover"
+        className=" h-80 w-full bg-center object-cover"
       />
       <div className="absolute top-0 right-0 bg-black/20 z-2 h-full w-full"></div>
       <p className="absolute bottom-5 left-5 text-xl font-semibold text-white">
